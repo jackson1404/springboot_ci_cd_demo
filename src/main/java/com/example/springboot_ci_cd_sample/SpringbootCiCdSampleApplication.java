7d1e5911d87add2d012efd320e92a8e79aea9444
25-00313 <jackson14042004@gmail.com>
2025-09-29 15:17:25 +0630

    test2

--- src/main/java/com/example/springboot_ci_cd_sample/SpringbootCiCdSampleApplication.java
+++ src/main/java/com/example/springboot_ci_cd_sample/SpringbootCiCdSampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootCiCdSampleApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello Again");
 		SpringApplication.run(SpringbootCiCdSampleApplication.class, args);
 	}
 
