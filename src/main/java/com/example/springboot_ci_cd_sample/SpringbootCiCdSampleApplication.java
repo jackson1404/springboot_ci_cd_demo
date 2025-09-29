84ffc3015828ff0b4bd82709fbd3fa19cc0c80b2
25-00313 <jackson14042004@gmail.com>
2025-09-29 15:09:56 +0630

    first commit

---
+++ src/main/java/com/example/springboot_ci_cd_sample/SpringbootCiCdSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_ci_cd_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCiCdSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCiCdSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_ci_cd_sample/SpringbootCiCdSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_ci_cd_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCiCdSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
